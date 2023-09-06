2c1e2fd907d2a5090f5897975e63ad3baa958357
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-09-06 17:20:40 -0400

    marker scale

--- src/components/projectMap.js
+++ src/components/projectMap.js
@@ -11,13 +11,37 @@ import { Link } from "gatsby"
 import ProjectTile from "./projectTile"
 
 const ProjectMap = ({ projects }) => {
-  const [zoomLevel, setZoomLevel] = useState(1)
+  const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 })
+  const [scaleFactor, setScaleFactor] = useState(1)
   const cleanedData = projects.filter(project => project.exactLocation !== null)
+
+  function handleZoomIn() {
+    if (position.zoom >= 8) return
+    setPosition(pos => ({ ...pos, zoom: pos.zoom * 1.2 }))
+    setScaleFactor(scale => scale * 1.2)
+  }
+
+  function handleZoomOut() {
+    if (position.zoom <= 1) return
+    setPosition(pos => ({ ...pos, zoom: pos.zoom / 1.2 }))
+    setScaleFactor(scale => scale / 1.2)
+  }
+
+  function handleMoveEnd(position) {
+    setPosition(position)
+  }
 
   return (
     <div className="projects-map-container">
       <ComposableMap>
-        <ZoomableGroup zoom={zoomLevel}>
+        <ZoomableGroup
+          zoom={position.zoom}
+          center={position.coordinates}
+          onMove={({ zoom }) => {
+            setScaleFactor(zoom)
+          }}
+          onMoveEnd={handleMoveEnd}
+        >
           <Geographies geography="https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json">
             {({ geographies }) =>
               geographies.map(geo => (
@@ -44,35 +68,29 @@ const ProjectMap = ({ projects }) => {
               data-tooltip-id={project.id}
             >
               <Link to={`/projects/${project.slug}`}>
-                <circle r={2} className="map-marker" />
+                <circle r={2 / scaleFactor} className="map-marker" />
               </Link>
             </Marker>
           ))}
         </ZoomableGroup>
       </ComposableMap>
       <div className="zoom-button-container">
-        <button
-          className="zoom-button"
-          onClick={() => setZoomLevel(prev => prev + 1)}
-        >
+        <button className="zoom-button" onClick={handleZoomIn}>
           +
         </button>
-        <button
-          className="zoom-button"
-          onClick={() => {
-            if (zoomLevel > 1) {
-              setZoomLevel(prev => prev - 1)
-            } else {
-              return null
-            }
-          }}
-        >
+        <button className="zoom-button" onClick={handleZoomOut}>
           -
         </button>
       </div>
       {cleanedData.map(project => (
-        <Tooltip id={project.id} key={project.id} place="bottom" clickable className="map-tooltip">
-            <ProjectTile project={project}></ProjectTile>
+        <Tooltip
+          id={project.id}
+          key={project.id}
+          place="bottom"
+          clickable
+          className="map-tooltip"
+        >
+          <ProjectTile project={project}></ProjectTile>
         </Tooltip>
       ))}
     </div>
